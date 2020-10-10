59ee1e3a4c662d584072c52d8851660e0fab5936
Home iMac <homeimac@Homes-iMac.local>
2020-10-10 15:47:24 +0545

    added coco_cv2.py

--- tools/trainv4.py
+++ tools/trainv4.py
@@ -19,6 +19,7 @@ from torch.utils.data import DataLoader
 from lib.models import model_factory
 from configs import cfg_factory
 from lib.cityscapes_cv2 import get_data_loader
+from lib.coco_cv2 import get_data_loader
 from tools.evaluate import eval_model
 from lib.ohem_ce_loss import OhemCELoss
 from lib.lr_scheduler import WarmupPolyLrScheduler
